17ae64644ecb406e0ee9cea6d1b16e187357d589
kiu <quentin.alamelou@gmail.com>
2025-06-27 18:54:04 +0100

    refine build pipeline

--- src/main/java/org/springframework/samples/petclinic/PetClinicRuntimeHints.java
+++ src/main/java/org/springframework/samples/petclinic/PetClinicRuntimeHints.java
@@ -26,7 +26,7 @@ public class PetClinicRuntimeHints implements RuntimeHintsRegistrar {
 
 	@Override
 	public void registerHints(RuntimeHints hints, ClassLoader classLoader) {
-		hints.resources().registerPattern("db/*"); // https://github.com/spring-projects/spring-boot/issues/32654
+		hints.resources().registerPattern("QAdb/*"); // https://github.com/spring-projects/spring-boot/issues/32654
 		hints.resources().registerPattern("messages/*");
 		hints.resources().registerPattern("mysql-default-conf");
 		hints.serialization().registerType(BaseEntity.class);
